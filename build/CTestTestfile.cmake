ebe56f8e6fa59f946c5195634ebb8a42d3c4e051
Ruben Perez <perezben37@gmail.com>
2021-02-02 19:35:30 -0800

    adding other folders

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rubaduben/github_robots/row_crop_navigator/src
+# Build directory: /home/rubaduben/github_robots/row_crop_navigator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rubaduben/github_robots/row_crop_navigator/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
